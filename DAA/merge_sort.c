9068acf7f99b94e627390cc381f8c3a95ce4bb6f
Deeptanshu Lal <143280861+Lazyplayer00@users.noreply.github.com>
2024-02-15 11:18:05 +0000

    fixed merge sort and knapsack

--- DAA/merge_sort.c
+++ DAA/merge_sort.c
@@ -1,18 +1,74 @@
 #include <stdio.h>
 
-int printArray(int arr[],int size){
-    int i = 0;
+void PrintArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
-    for(i=0; i<5 ;i++){
-        printf("%d",arr[i]);
+void Combine(int A[], int low, int mid, int high) {
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+    int temp[high - low + 1];
+
+    while (i <= mid && j <= high) {
+        if (A[i] <= A[j]) {
+            temp[k] = A[i];
+            i++;
+        } else {
+            temp[k] = A[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid) {
+        temp[k] = A[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high) {
+        temp[k] = A[j];
+        j++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++) {
+        A[x] = temp[x];
     }
 }
 
-int main(){
+void MergeSort(int A[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        MergeSort(A, low, mid);
+        MergeSort(A, mid + 1, high);
+        Combine(A, low, mid, high);
+    }
+}
 
-    int arr[]={2,3,1,7,9};
-    int size=5;
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
 
-    printArray(arr,size);
+    int arr[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Element array[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    PrintArray(arr, size);
+
+    MergeSort(arr, 0, size - 1);
+
+    printf("Sorted array: ");
+    PrintArray(arr, size);
+
     return 0;
 }
