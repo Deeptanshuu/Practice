7277fe1f0ff341192777da31bfb225e00b16e9ee
Deeptanshu Lal <143280861+Lazyplayer00@users.noreply.github.com>
2024-03-12 05:05:23 +0000

    stopandsliding

--- CN/exp-7/exp-7.c
+++ CN/exp-7/exp-7.c
@@ -1,35 +1,69 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main() {
-    int w, i, f, frames[50];
-
-    printf("Enter the window size: ");
-    scanf("%d", &w);
-
-    printf("\nEnter the number of frames to transmit: ");
-    scanf("%d", &f);
-
-    printf("\nEnter %d frames:", f);
-    for(i = 1; i <= f; i++) {
-        scanf("%d", &frames[i]);
+void stopAndWait() {
+    int frame[10], i, j, n;
+    printf("Enter the number of frames: ");
+    scanf("%d", &n);
+    printf("Enter the frame sequence:\n");
+    for(i=1; i<=n; i++) {
+        scanf("%d", &frame[i]);
     }
-
-    printf("With sliding window protocol the frames will be sent in the following manner (assuming no corruption of frames)\n\n");
-    printf("After sending %d frames at each stage sender waits for acknowledgement sent by the receiver\n\n", w);
-
-    for(i = 1; i <= f; i++) {
-        if(i % w == 0) {
-            printf("%d\n", frames[i]);
-            printf("Acknowledgment of above frames sent is received by sender\n\n");
+    i=1;
+    while(i<=n) {
+        printf("Frame %d has been transmitted.\n", frame[i]);
+        printf("Did you receive frame %d? (0=NO, 1=YES): ", frame[i]);
+        scanf("%d", &j);
+        if(j==0) {
+            printf("Frame %d will be retransmitted.\n", frame[i]);
         } else {
-            printf("%d ", frames[i]);
+            i++;
         }
     }
+    printf("\nAll frames have been transmitted successfully.\n");
+}
 
-    if(f % w != 0) {
-        printf("\n Acknowledgment of above frames sent is received by sender\n");
+void slidingWindow() {
+    int w, i, f, frames[50];
+    printf("Enter window size: ");
+    scanf("%d", &w);
+    printf("Enter number of frames to transmit: ");
+    scanf("%d", &f);
+    printf("Enter %d frames: ", f);
+    for(i=1; i<=f; i++) {
+        scanf("%d", &frames[i]);
     }
+    printf("\nWith sliding window protocol the frames will be sent in the following manner (assuming no corruption of frames)\n\n");
+    printf("After sending %d frames at each stage sender waits for acknowledgement sent by the receiver\n\n", w);
+    for(i=1;i<=f;i++) {
+        if(i%w==0) {
+            printf("%d\n",frames[i]);
+            printf("Acknowledgement of above frames sent is received by sender\n\n");
+        } else {
+            printf("%d ",frames[i]);
+        }
+    }
+    if(f%w!=0) {
+        printf("\nAcknowledgement of above frames sent is received by sender\n");
+    }
+}
 
-    getchar();
+int main() {
+    int choice;
+    printf("Choose a protocol to implement:\n");
+    printf("1. Stop and Wait Protocol\n");
+    printf("2. Sliding Window Protocol\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch(choice) {
+        case 1:
+            stopAndWait();
+            break;
+        case 2:
+            slidingWindow();
+            break;
+        default:
+            printf("Invalid choice.\n");
+    }
     return 0;
 }
