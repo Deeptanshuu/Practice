be143795fbb3b23dd9eda686af638a372949de38
Deeptanshu Lal <143280861+Deeptanshuu@users.noreply.github.com>
2024-07-10 16:22:46 +0530

    Add files via upload

--- python/exp1.py
+++ python/exp1.py
@@ -1,4 +1,27 @@
-print("Hello World !!!!!!!!!!!!!")
+print(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Exit")
+
+
+ch = int(input("Enter Your Choice: "))
+
+a  = float(input("Enter first value: "))
+b  = float(input("Enter second value: "))
+
+
+if ch == 1:
+	print("Answer of %.2f + %.2f = %.2f"%(a , b, a+b))
+
+elif ch==2:
+	print("Answer of %.2f - %.2f = %.2f"%(a, b, a-b))
+
+elif ch==3:
+	print("Answer of %.2f x %.2f = %.2f"%(a, b, a*b))
+
+elif ch==4:
+	print("Answer of %.2f / %.2f = %.2f"%(a, b, a/b))
+
+else:
+	print("BYE")
+
 
 
 
